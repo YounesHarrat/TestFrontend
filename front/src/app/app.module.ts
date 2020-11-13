2c26cdf3b5a2217b058f424ee86f09e762e0e888
younes.harrat <harratanis@hotmail.com>
2020-11-13 10:54:00 +0100

    set structure

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ArticlepageComponent } from './articlepage/articlepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ArticlepageComponent
   ],
   imports: [
     BrowserModule,
